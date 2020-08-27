625b0d9edddffe0ca52f7f10eee6e0e26a510dd1
abarpan3 <abarpan3@gmail.com>
2020-08-27 15:57:04 +0530

    Add files via upload

---
+++ JS LAB/LAB 1/Hello.js
@@ -0,0 +1,5 @@
+function dispHello(){
+
+    return "Hello World"
+    
+}
---
+++ JS LAB/LAB 1/common.js
@@ -0,0 +1,10 @@
+var msg;
+msg="<p><code>The actual script is in external script file called common.js</code></p>";
+
+function addNos(headVar,bodyVar)
+{
+//TODO: display the contents of the variable "msg"
+    document.write(msg)
+//TODO: display the addition of two numbers
+    document.write("The sum of headVar and bodyVar is "+ ((Number(headVar))+Number(bodyVar)))
+}
